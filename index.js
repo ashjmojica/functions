75d99606d0a46ad81663eacea9b3cb7b4b14ddc4
Ashley Mojica <ashjmojica@gmail.com>
2023-05-01 21:29:54 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+//Write a function that takes two numbers as arguments and returns their sum.
+
+//Write a function that takes a string as an argument and returns the length of the string.
+
+//Write a function that takes an array of numbers as an argument and returns the largest number in the array.
+
+//Write a function that takes an array of strings as an argument and returns a new array with all the strings capitalized.
+
+//Write a function that takes a number as an argument and returns true if the number is even, and false if it is odd.
+
+//Write a function that takes two arrays as arguments and returns a new array that contains all the elements from both arrays, in the order they appear.
+
+//Write a function that takes a string as an argument and returns a new string with all the vowels removed.
+
+//Write a function that takes an array of numbers as an argument and returns a new array with all the numbers doubled.
+
+//Write a function that takes an array of strings as an argument and returns a new array with all the strings sorted in alphabetical order.
+
+//Write a function that takes a number as an argument and returns the factorial of that number (i.e., the product of all the numbers from 1 to that number).
